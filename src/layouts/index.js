683e8ca43f04613e3afdbecd43b56a07650a4688
Paul Barnes <paul.barnes.psnl@gmail.com>
2019-02-17 23:18:06 +0000

    add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,8 +11,15 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'msapplication-TileColor', content: '#da532c' },
+        { name: 'theme-color', content: '#ffffff' },
+      ]}
+      link={[
+        { rel: 'apple-touch-icon', sizes: '180x180', href: `/apple-touch-icon.png` },
+        { rel: 'icon', sizes: '32x32', href: `/favicon-32x32.png` },
+        { rel: 'icon', sizes: '16x16', href: `/favicon-16x16.png` },
+        { rel: 'manifest', href: `/site.webmanifest` },
+        { rel: 'mask-icon', href: `/safari-pinned-tab.svg`, color: `#5bbad5` },
       ]}
     />
     <Header/>
