3e722639391adf820fed02cd419cfdf7d254c2b5
Paul Barnes <paul.barnes.psnl@gmail.com>
2026-05-13 09:38:40 -0500

    update profile image

--- src/components/portrait.jsx
+++ src/components/portrait.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import portraitImage from '../assets/images/profile-1.webp'
+import React from "react";
+import portraitImage from "../assets/images/profile.webp";
 
 class Portrait extends React.Component {
-    render() {
-        return (
-            <div className="portrait fade-in">
-                <img src={portraitImage} height="234" width="234" alt="test"/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="portrait fade-in">
+        <img src={portraitImage} height="234" width="234" alt="test" />
+      </div>
+    );
+  }
 }
 
-export default Portrait
+export default Portrait;
